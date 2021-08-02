084f58e939fa6ed399ec403c1a0e5011a722d5f5
Wendy Mudzinski <wendymud@gmail.com>
2021-08-01 23:30:44 -0700

    initial creation of repo

---
+++ Resources/maps.js
@@ -0,0 +1,81 @@
+// var numbers = [1,2,3,4,5];
+// var doubled = numbers.map(function(num){
+//     return num * 2;
+// });
+// console.log(doubled);
+
+// var numbers = [1,2,3,4,5];
+// var add5 = numbers.map(function(num){
+//     return num + 5;
+// });
+// console.log(add5)
+
+// var cities = [
+//     {
+//       "Rank": 1,
+//       "City": "San Antonio ",
+//       "State": "Texas",
+//       "Increase_from_2016": "24208",
+//       "population": "1511946"
+//     },
+//     {
+//       "Rank": 2,
+//       "City": "Phoenix ",
+//       "State": "Arizona",
+//       "Increase_from_2016": "24036",
+//       "population": "1626078"
+//     },
+//     {
+//       "Rank": 3,
+//       "City": "Dallas",
+//       "State": "Texas",
+//       "Increase_from_2016": "18935",
+//       "population": "1341075"
+//     }
+// ];
+
+// var cityNames = cities.map(function(loc){
+//     return loc.City;
+// });
+// console.log(cityNames);
+
+// var cityNames = cities.map(function(peeps){
+//     return peeps.population;
+// });
+// console.log(cityNames);
+
+// var numbers = [1,2,3,4,5];
+
+// var larger = numbers.filter(function(num){
+//     return num > 1;
+// });
+
+// console.log(larger);
+
+// var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
+// var speciesS = words.filter(function(sss){
+//     return sss.startsWith('s');
+// })
+// console.log(speciesS);
+
+// var numbers = [1,2,3,4,5];
+
+// var doubled = numbers.map(num => num * 2);
+// console.log(doubled);
+
+// var familyAge = [3,2,39,37,9];
+// var sortedAge = familyAge.sort((a,b) => a - b);
+// console.log(sortedAge);
+
+// var familyAge = [3,2,39,37,9];
+// sortedAge = familyAge.sort((a,b) => b - a);
+// console.log(sortedAge);
+
+// var integers = [0,1,2,3,4,5];
+// var slice1 = integers.slice(0,2);
+// console.log(slice1);
+
+var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
+var slice2 = words.slice(3,);
+console.log(slice2);
+
---
+++ Resources/plots.js
@@ -0,0 +1,63 @@
+//Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+// var trace = {
+//     x: ["burrito", "pizza", "chicken"],
+//     y: [10, 18, 5],
+//     type: "bar"
+// };
+
+// var layout = {
+//     title: "Luncheon Survey",
+//     xaxis: {title: "Food Option"},
+//     yaxis: {title: "Number of Respondents"}
+// };
+// Plotly.newPlot("plotArea", [trace], layout);
+
+// var trace = {
+//     x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+//     y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+//     type: "bar"
+//    };
+//    var data = [trace];
+//    var layout = {
+//     title: "'Bar' Chart",
+//     xaxis: { title: "Drinks"},
+//     yaxis: { title: "% of Drinks Ordered"}
+//    };
+//    Plotly.newPlot("plotArea", data, layout);
+
+// var trace = {
+//     labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita",
+//     "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+//     values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+//     type: 'pie'
+//    };
+//    var data = [trace];
+//    var layout = {
+//     title: "'Pie' Chart",
+//    };
+//    Plotly.newPlot("plotArea", data, layout);
+
+var trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    mode: 'markers',
+    type: 'scatter'
+  };
+  
+  var trace2 = {
+    x: [2, 3, 4, 5],
+    y: [16, 5, 11, 9],
+    mode: 'lines',
+    type: 'scatter'
+  };
+  
+  var trace3 = {
+    x: [1, 2, 3, 4],
+    y: [12, 9, 15, 12],
+    mode: 'lines+markers',
+    type: 'scatter'
+  };
+  
+  var data = [trace1, trace2, trace3];
+  
+  Plotly.newPlot('plotArea', data);
